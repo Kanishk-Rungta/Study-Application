4ab123486c10f84ec66082c79416932cd12ebaea
Kanishk-Rungta <rungtakanishk@gmail.com>
2026-03-14 12:55:34 +0530

    Time Check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ const SCHEDULE = [
   { day: "Wednesday", time: "1:00 PM - 8:00 PM", h: 13, m: 0 },
   { day: "Thursday", time: "Break", h: null, m: null },
   { day: "Friday", time: "9:00 AM - 8:00 PM", h: 9, m: 0 },
-  { day: "Saturday", time: "12:00 PM - 8:00 PM", h: 12, m: 0 },
+  { day: "Saturday", time: "9:00 PM - 8:00 PM", h: 9, m: 0 },
   { day: "Sunday", time: "1:00 PM - 8:00 PM", h: 13, m: 0 }
 ];
 
